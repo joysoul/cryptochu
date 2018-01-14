0ddbe86dad9ddb1a2dd4732cb3f0ae9b923155f2
Emile Pels <mail@emilepels.nl>
2018-01-14 13:53:35 +0100

    Added base test

---
+++ tests/TestCase.php
@@ -0,0 +1,13 @@
+<?php
+namespace cryptochu\tests;
+
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+/**
+ * @author Emile Pels
+ * @package cryptochu
+ */
+abstract class TestCase extends BaseTestCase
+{
+
+}
