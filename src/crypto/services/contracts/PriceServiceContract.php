6b45d626866517174b60fc93727e3804688c5a84
Emile Pels <mail@emilepels.nl>
2018-01-20 17:12:29 +0100

    PriceServiceContract now distinguishes between ask and bid prices

--- src/crypto/services/contracts/PriceServiceContract.php
+++ src/crypto/services/contracts/PriceServiceContract.php
@@ -5,10 +5,28 @@ use cryptochu\crypto\currencies\CryptoCurrency;
 use cryptochu\money\MoneyAmount;
 
 /**
+ * PriceServiceContract implementations fetch ask and bid prices for a provided cryptocurrency.
+ *
  * @author Emile Pels
  * @package cryptochu\crypto\services\contracts
  */
 interface PriceServiceContract extends ExchangeServiceContract
 {
-    public function getPrice(CryptoCurrency $cryptoCurrency): MoneyAmount;
+    /**
+     * Get the ask price for the given cryptocurrency.
+     *
+     * @param CryptoCurrency $cryptoCurrency
+     *
+     * @return MoneyAmount
+     */
+    public function getAsk(CryptoCurrency $cryptoCurrency): MoneyAmount;
+
+    /**
+     * Get the bid price for the given cryptocurrency.
+     *
+     * @param CryptoCurrency $cryptoCurrency
+     *
+     * @return MoneyAmount
+     */
+    public function getBid(CryptoCurrency $cryptoCurrency): MoneyAmount;
 }
