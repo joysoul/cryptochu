c5ed08d7291719d33a0f1702b117cee3f60b15fa
Emile Pels <mail@emilepels.nl>
2018-01-24 15:31:30 +0100

    Moved mock helpers to TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,6 +1,9 @@
 <?php
 namespace cryptochu\tests;
 
+use cryptochu\http\contracts\HttpClient;
+use cryptochu\services\contracts\LoggerServiceContract;
+use PHPUnit\Framework\MockObject\Matcher\Invocation;
 use PHPUnit\Framework\TestCase as BaseTestCase;
 
 /**
@@ -9,5 +12,41 @@ use PHPUnit\Framework\TestCase as BaseTestCase;
  */
 abstract class TestCase extends BaseTestCase
 {
+    /**
+     * Mock constants.
+     */
+    const METHOD_HTTP_CLIENT_GET_CONTENT = 'getContent';
+    const METHOD_LOGGER_SERVICE_INFO = 'info';
 
+    /**
+     * Gets a HttpClient mock that returns the content provided.
+     *
+     * @param string $content
+     *
+     * @return HttpClient
+     */
+    protected function getHttpClientThatReturnsContent(string $content): HttpClient
+    {
+        $httpClient = static::createMock(HttpClient::class);
+        $httpClient->method(self::METHOD_HTTP_CLIENT_GET_CONTENT)->willReturn($content);
+
+        /** @var HttpClient $httpClient */
+        return $httpClient;
+    }
+
+    /**
+     * Gets a LoggerService mock that registers the provided invocation expectation.
+     *
+     * @param Invocation $matcher
+     *
+     * @return LoggerServiceContract
+     */
+    protected function getMockLoggerServiceThatIsCalled(Invocation $matcher): LoggerServiceContract
+    {
+        $mockLoggerService = static::createMock(LoggerServiceContract::class);
+        $mockLoggerService->expects($matcher)->method(self::METHOD_LOGGER_SERVICE_INFO);
+
+        /** @var LoggerServiceContract $mockLoggerService */
+        return $mockLoggerService;
+    }
 }
