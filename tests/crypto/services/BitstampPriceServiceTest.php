c829316a32a16b112c760ea1fcae252dca9b07c0
Emile Pels <mail@emilepels.nl>
2018-01-24 15:43:35 +0100

    Bitstamp now uses logger

--- tests/crypto/services/BitstampPriceServiceTest.php
+++ tests/crypto/services/BitstampPriceServiceTest.php
@@ -4,13 +4,13 @@ namespace cryptochu\crypto\services\tests;
 use cryptochu\crypto\currencies\CryptoCurrency;
 use cryptochu\crypto\services\BitstampPriceService;
 use cryptochu\money\MoneyAmountBuilder;
-use cryptochu\tests\HttpTestCase;
+use cryptochu\tests\TestCase;
 
 /**
  * @author Emile Pels
  * @package cryptochu\crypto\services\tests
  */
-class BitstampPriceServiceTest extends HttpTestCase
+class BitstampPriceServiceTest extends TestCase
 {
     /**
      * Fake response.
@@ -19,16 +19,32 @@ class BitstampPriceServiceTest extends HttpTestCase
         . '"vwap": "12375.06", "volume": "10203.66876269", "low": "11250.00", "ask": "12828.03", "open": "11560.82"}';
 
     /**
+     * @var BitstampPriceService
+     */
+    private $bitstampPriceService;
+
+    /**
+     *
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->bitstampPriceService = new BitstampPriceService(
+            $this->getHttpClientThatReturnsContent(self::FAKE_RESPONSE),
+            $this->getMockLoggerServiceThatIsCalled(static::once())
+        );
+    }
+
+    /**
      *
      */
     public function testGetAsk()
     {
-        $httpClient = $this->getHttpClientThatReturnsContent(self::FAKE_RESPONSE);
-
-        $bitfinexPriceService = new BitstampPriceService($httpClient);
-        $ask = $bitfinexPriceService->getAsk(CryptoCurrency::bitcoin());
-
-        static::assertEquals(MoneyAmountBuilder::fromUnitedStatesDollar()->amount(12828, 3)->build(), $ask);
+        static::assertEquals(
+            MoneyAmountBuilder::fromUnitedStatesDollar()->amount(12828, 3)->build(),
+            $this->bitstampPriceService->getAsk(CryptoCurrency::bitcoin())
+        );
     }
 
     /**
@@ -36,11 +52,9 @@ class BitstampPriceServiceTest extends HttpTestCase
      */
     public function testGetBid()
     {
-        $httpClient = $this->getHttpClientThatReturnsContent(self::FAKE_RESPONSE);
-
-        $bitfinexPriceService = new BitstampPriceService($httpClient);
-        $bid = $bitfinexPriceService->getBid(CryptoCurrency::bitcoin());
-
-        static::assertEquals(MoneyAmountBuilder::fromUnitedStatesDollar()->amount(12800, 33)->build(), $bid);
+        static::assertEquals(
+            MoneyAmountBuilder::fromUnitedStatesDollar()->amount(12800, 33)->build(),
+            $this->bitstampPriceService->getBid(CryptoCurrency::bitcoin())
+        );
     }
 }
