590a355436458f9c721910100ed0b8064f58fde6
Emile Pels <mail@emilepels.nl>
2018-01-27 15:50:19 +0100

    Added reference to PSR-3 docs to LoggingServiceContract

--- src/services/contracts/LoggingServiceContract.php
+++ src/services/contracts/LoggingServiceContract.php
@@ -4,6 +4,9 @@ namespace cryptochu\services\contracts;
 use Psr\Log\LoggerInterface;
 
 /**
+ * LoggingServiceContract is currently equivalent to the PHP-FIG's PSR-3 LoggerInterface.
+ * See: http://www.php-fig.org/psr/psr-3/
+ *
  * @author Emile Pels
  * @package cryptochu\services\contracts
  */
